24d6d612d800ea323d41f33338a8e68dff6222ae
nishitamodh <ngre8t@gmail.com>
2025-05-24 15:18:07 +0530

    Updated server.py

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,6 @@
+import os
 from waitress import serve
 from app import app  # Ensure this matches your Flask app file
 
-serve(app, host="0.0.0.0", port=8000)
+port = int(os.environ.get("PORT", 8000))
+serve(app, host="0.0.0.0", port=port)
